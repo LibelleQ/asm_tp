419ff65d74339a68d7c9de31dac1d199aeb7d68f
LibelleQ <aymerictran75@gmail.com>
2025-09-29 10:07:36 -0400

    test

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,34 +1,61 @@
+section .bss
+	buffer resb 64
+
 section .data
 	msg db '1337'
-	buffer times 3 db 0
+	msglen equ $-msg
 section .text
 
 	global _start
 
 _start:
-
 	mov rax, 0
 	mov rdi, 0
 	mov rsi, buffer
-	mov rdx, 3
+	mov rdx, 64
 	syscall
 
-	mov al, [buffer]
-	cmp al, '4'
-	jne _exit
+	test rax, rax
+	jle _exit_fail
 
+	cmp rax, 2
+	je _check2
+	cmp rax, 3
+	je _check3
+	jmp _exit_fail
+
+_check2:
+	mov al,[buffer]
+	cmp al, '4'
+	jne _exit_fail
 	mov al, [buffer+1]
 	cmp al, '2'
-	jne _exit
+	jne _exit_fail
 
+_check3:
+	mov al, [buffer]
+	cmp al, '4'
+	jne _exit_fail
+	mov al, [buffer+1]
+	cmp al, '2'
+	jne _exit_fail
+	cmp byte [buffer+2],10
+	jne _exit_fail
+	jmp sucess
+
+sucess:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, msg
-	mov rdx, 4
+	mov rdx, msglen
 	syscall
 
-_exit:
 	mov rax, 60
-	mov rdi, 0
+	xor rdi, rdi
+	syscall
+
+_exit_fail:
+	mov rax, 60
+	mov rdi, 1
 	syscall
 
